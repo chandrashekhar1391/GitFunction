e7f8cd8c78a2bfb3b9a43604708177f67e393144
Dell <Dell@DESKTOP-G8FGSQD>
2025-02-12 13:33:55 +0530

    push

---
+++ TestingGitFunction/src/test/java/Contact/ContactPage.java
@@ -0,0 +1,10 @@
+package Contact;
+
+public class ContactPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Testing Git Function for Contact");
+	}
+
+}
---
+++ TestingGitFunction/src/test/java/Org/OrgPage.java
@@ -0,0 +1,10 @@
+package Org;
+
+public class OrgPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Testing Git Function for Org");
+	}
+
+}
